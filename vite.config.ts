b455894f909b88aa2cd0544c471cf9ec9a476f68
basim amin bazaz <68185046+thehoneybug@users.noreply.github.com>
2025-06-28 13:37:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/bounce-alert-landing/', 
   server: {
     host: "::",
     port: 8080,
